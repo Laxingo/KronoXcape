00cf4044a17ad367279a1671cf43430fdd5e2528
samfernandes03 <samuel.pinho.fernandes@gmail.com>
2023-06-04 19:36:17 +0100

    creation of init file

---
+++ js/init.js
@@ -0,0 +1,19 @@
+init();
+
+function init() {
+  // USERS
+  if (!localStorage.users) {
+    const users = [
+      {
+        username: "user1",
+        password: "pass1",
+      },
+      {
+        username: "user2",
+        password: "pass2",
+      },
+    ];
+    console.log("inject");
+    localStorage.setItem("users", JSON.stringify(users));
+  }
+}
