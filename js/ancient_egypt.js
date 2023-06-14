0f0dcfa36329707ceb148434cff86b4db8bfdc3a
Sdanielgg <40220220@esmad.ipp.pt>
2023-06-14 14:52:02 +0100

    temporizador e criação do ancient_egypt.js

---
+++ js/ancient_egypt.js
@@ -0,0 +1,42 @@
+function startTimer() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
+  window.onload = function() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+  }
+  let timerInterval;
+let remainingTime = 900; // 15 minutes in seconds
+
+function startTimer() {
+  if (timerInterval) return; 
+  timerInterval = setInterval(updateTimer, 1000);
+}
+
+function updateTimer() {
+  const minutes = Math.floor(remainingTime / 60);
+  const seconds = remainingTime % 60;
+  const formattedTime = formatTime(minutes, seconds);
+  document.getElementById("timer").textContent = formattedTime;
+
+  if (remainingTime <= 0) {
+    clearInterval(timerInterval);
+    alert("Timer completed!");
+  } else {
+    remainingTime--;
+  }
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+window.onload = function() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+
+
+function formatTime(minutes, seconds) {
+  return (
+    String(minutes).padStart(2, "0") + ":" + String(seconds).padStart(2, "0")
+  );
+}
