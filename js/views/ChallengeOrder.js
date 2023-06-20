2b1724f630d5e459549b446654ae14617b4ce8da
Sdanielgg <40220220@esmad.ipp.pt>
2023-06-20 23:50:54 +0100

    novo ficheiro js

---
+++ js/views/ChallengeOrder.js
@@ -0,0 +1,17 @@
+var Pyramid1Element=document.getElementById("pyramid1");
+var Pyramid2Element=document.getElementById("pyramid2");
+function pyramidshow(){
+if (pyramids>0){
+    Pyramid1Element.style.display="block"
+}
+else if(pyramids==2){
+    Pyramid2Element.style.display="block"
+
+}}
+function blockSP(){
+    if (pyramids>=3){
+        //unblock sliding puzzle
+
+    }
+}
+
