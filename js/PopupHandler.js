eae399a62f5413a865f66cf550837423ecee026d
samfernandes03 <samuel.pinho.fernandes@gmail.com>
2023-06-21 01:53:07 +0100

    minor changes and fixes

--- js/PopupHandler.js
+++ js/PopupHandler.js
@@ -1,41 +1,54 @@
 function openFillInPopup() {
-    const width = 700;
-    const height = 400;
-    const left = (window.innerWidth - width) / 2;
-    const top = (window.innerHeight - height) / 2;
-    const options = `width=${width},height=${height},left=${left},top=${top}`;
-    const fillinWindow = window.open('fill.html', '_blank', options);
+  const width = 800;
+  const height = 450;
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+  const options = `width=${width},height=${height},left=${left},top=${top}`;
+  const fillinWindow = window.open('fill.html', '_blank', options);
 
-    fillinWindow.onbeforeunload = function () {
-      
-      setTimeout(() => {
-        var progressBar = document.querySelector('.progress-bar');
-        var currentWidth = parseInt(progressBar.style.width, 10);
-        var newWidth = currentWidth + 20;
-        progressBar.style.width = newWidth + '%';
-        progressBar.setAttribute('aria-valuenow', newWidth); 
-      });
-    };
-  }
-  function openQuestionsPopup() {
-    const width = 700;
-    const height = 400;
-    const left = (window.innerWidth - width) / 2;
-    const top = (window.innerHeight - height) / 2;
-    const options = `width=${width},height=${height},left=${left},top=${top}`;
-    const questionsWindow = window.open('questions.html', '_blank', options);
+  fillinWindow.onbeforeunload = function () {
+    
+    setTimeout(() => {
+      var progressBar = document.querySelector('.progress-bar');
+      var currentWidth = parseInt(progressBar.style.width, 10);
+      var newWidth = currentWidth + 20;
+      progressBar.style.width = newWidth + '%';
+      progressBar.setAttribute('aria-valuenow', newWidth); 
 
-    questionsWindow.onbeforeunload = function () {
+      // Display the pyramid image in the first inventory square
+      document.getElementById('pyramid2').style.display = 'inline';
       
-      setTimeout(() => {
-        var progressBar = document.querySelector('.progress-bar');
-        var currentWidth = parseInt(progressBar.style.width, 10);
-        var newWidth = currentWidth + 20;
-        progressBar.style.width = newWidth + '%';
-        progressBar.setAttribute('aria-valuenow', newWidth); 
-      });
-    };
-  }
+      // Disable the event listener to prevent further popups
+      document.getElementById('fillin-popup').removeEventListener('click', openFillInPopup);
+    }, 50);
+  };
+}
+
+function openQuestionsPopup() {
+  const width = 700;
+  const height = 400;
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+  const options = `width=${width},height=${height},left=${left},top=${top}`;
+  const questionsWindow = window.open('questions.html', '_blank', options);
+
+  questionsWindow.onbeforeunload = function () {
+    setTimeout(() => {
+      var progressBar = document.querySelector('.progress-bar');
+      var currentWidth = parseInt(progressBar.style.width, 10);
+      var newWidth = currentWidth + 20; 
+      progressBar.style.width = newWidth + '%';
+      progressBar.setAttribute('aria-valuenow', newWidth); 
+
+      // Display the pyramid image in the first inventory square
+      document.getElementById('pyramid1').style.display = 'inline';
+      
+      // Disable the event listener to prevent further popups
+      document.getElementById('questions-popup').removeEventListener('click', openQuestionsPopup);
+    }, 50);
+  };
+}
+
   function openPopup() {
       const width = 400;
       const height = 400;
